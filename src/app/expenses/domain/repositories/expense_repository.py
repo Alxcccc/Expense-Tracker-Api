c3baeb7b2d323047c2135d4c919f0b480d38523f
Alxc <alecandres799@gmail.com>
2025-02-07 11:07:59 -0500

    repository expense

---
+++ src/app/expenses/domain/repositories/expense_repository.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional, Union
+
+from src.app.expenses.domain.entities.expense import Expense
+
+class ExpenseRepository(ABC):
+    
+    @abstractmethod
+    def get_all(self) -> List[Expense]:
+        pass
+    
+    @abstractmethod
+    def get_by_id(self, id_expense: int) -> Optional[Expense]:
+        pass
+    
+    @abstractmethod
+    def create(self, expense: Expense) -> Expense:
+        pass
+    
+    @abstractmethod
+    def update(self, id_expense: int, expense: Expense) -> Union[bool, Expense]:
+        pass
+    
+    @abstractmethod
+    def delete(self, id_expense: int) -> bool:
+        pass

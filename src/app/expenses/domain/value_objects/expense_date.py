1963d01a2c8d99505088424cee2d582376d272c9
Alxc <alecandres799@gmail.com>
2025-02-07 10:09:12 -0500

    validator expense date

--- src/app/expenses/domain/value_objects/expense_date.py
+++ src/app/expenses/domain/value_objects/expense_date.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel, Field, field_validator
-from datetime import date
+from datetime import datetime
 
 class ExpenseDate(BaseModel):
     value: str = Field(..., description="Date of expense")
     
     @field_validator("value")
     def validator_date(cls, value: str, format = "%Y-%m-%d"):
-        if not date.strftime(value, format):
-            raise ValueError("The date of expense not have correct format")
+        try:
+            datetime.strptime(value, format)
+        except ValueError:
+            raise ValueError(f"time data {value} does not match format {format}")
         
     def __eq__(self, other):
         return isinstance(other, ExpenseDate) and self.value == other.value

c2ebfb4f5dc1e54e852c7e8446ef77b9fa1c8555
Alxc <alecandres799@gmail.com>
2025-02-08 17:55:01 -0500

    refactor database class

--- src/app/infrastructure/database/database.py
+++ src/app/infrastructure/database/database.py
@@ -1,9 +1,10 @@
 from sqlmodel import create_engine
 
-class DataBaseEngine():
+class DataBase():
     DATABASE_URL = "mysql+pymysql://root:facebookalec7@localhost/expensetrackerapi"
     engine = None
     
+    @classmethod
     def create_engin(cls):
         if cls.engine is None:
             cls.engine = create_engine(cls.DATABASE_URL, echo=True)

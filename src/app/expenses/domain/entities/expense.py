7565e8064c86964b08290ec4f680aeee21ca7ff0
Alxc <alecandres799@gmail.com>
2025-02-08 15:34:19 -0500

    add new column: expense title

--- src/app/expenses/domain/entities/expense.py
+++ src/app/expenses/domain/entities/expense.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from src.app.expenses.domain.value_objects.expense_title import ExpenseTitle
 from src.app.expenses.domain.value_objects.expense_date import ExpenseDate
 from src.app.expenses.domain.value_objects.expense_description import ExpenseDescription
 from src.app.expenses.domain.value_objects.expense_category import ExpenseCategory
 from src.app.expenses.domain.value_objects.expense_amount import ExpenseAmount
 
 class Expense(BaseModel):
-    id: Optional[int]
+    id_expense: Optional[int]
+    expense_title: ExpenseTitle
     expense_date: ExpenseDate
     description: ExpenseDescription
     category: ExpenseCategory

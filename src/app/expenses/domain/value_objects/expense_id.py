a3602964ca8abf0a73c38e0ada42e57466937cd8
Alxc <alecandres799@gmail.com>
2025-02-10 14:58:34 -0500

    create validation for id expense

---
+++ src/app/expenses/domain/value_objects/expense_id.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field, field_validator
+
+class ExpenseId(BaseModel):
+    value: int = Field(..., description="Id of expense")
+    
+    @field_validator("value")
+    def validator_id(cls, value: int):
+        if value <= 0:
+            raise ValueError("This id is zero or less than zero")
+        
+    def __eq__(self, other):
+        return isinstance(other, ExpenseId) and self.value == other.value
+    
+    def __str__(self):
+        return self.value

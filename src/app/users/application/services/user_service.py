7c044dd436592a10df5db8157173f9823349066a
Alxc <alecandres799@gmail.com>
2025-02-16 18:04:28 -0500

    create file service.py for users

---
+++ src/app/users/application/services/user_service.py
@@ -0,0 +1,21 @@
+from typing import Optional, Union
+
+# Repositories
+from src.app.users.domain.repositories.user_repository import UsersRepository
+
+# Entities
+from src.app.users.domain.entities.user import User, UserCreate
+
+class UserService:
+    
+    def __init__(self, user_repository: UsersRepository):
+        self.user_repository = user_repository
+        
+    def get_by_id(self, id_user: int) -> Optional[User]:
+        return self.user_repository.get_by_id(id_user)
+    
+    def create(self, user: UserCreate) -> Union[User, bool]:
+        return self.user_repository.create(user)
+    
+    def delete(self, id_user: int) -> bool:
+        return self.user_repository.delete(id_user)

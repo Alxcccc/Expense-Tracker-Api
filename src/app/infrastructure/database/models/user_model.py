0020a3df114d406fbac831b4e80458c384e43607
Alxc <alecandres799@gmail.com>
2025-02-16 18:33:50 -0500

    refactor

--- src/app/infrastructure/database/models/user_model.py
+++ src/app/infrastructure/database/models/user_model.py
@@ -1,10 +1,9 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
-from datetime import date
 
 class UserDatabase(SQLModel, table=True):
     __tablename__ = "users"
     id_user: Optional[int] = Field(default=None, primary_key=True)
     username: str
-    password: date
+    password: str
     gmail: str

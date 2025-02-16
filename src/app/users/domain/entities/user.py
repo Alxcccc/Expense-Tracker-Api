4b144a0eb8d68384007f5042d95d51ec0fbb8836
Alxc <alecandres799@gmail.com>
2025-02-16 17:57:37 -0500

    add validator emailstr of pydantic

--- src/app/users/domain/entities/user.py
+++ src/app/users/domain/entities/user.py
@@ -1,17 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 from src.app.users.domain.value_objects.user_id import UserId
 from src.app.users.domain.value_objects.user_username import UserUsername
+from src.app.users.domain.value_objects.user_password import UserPassword
 
 class User(BaseModel):
     id_user = Optional[UserId]
     username = UserUsername
-    password = str
-    gmail = str
+    password = UserPassword
+    gmail = EmailStr
     
 class UserCreate(BaseModel):
-    username = str
-    password = str
-    gmail = str
+    username = UserUsername
+    password = UserPassword
+    gmail = EmailStr
     

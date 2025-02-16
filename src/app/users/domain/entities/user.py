9c6025d9b231f14dce784cd7d4765f382bc31ee4
Alxc <alecandres799@gmail.com>
2025-02-16 17:45:13 -0500

    add validator id user

--- src/app/users/domain/entities/user.py
+++ src/app/users/domain/entities/user.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from src.app.users.domain.value_objects.user_id import UserId
+
 class User(BaseModel):
-    id_user = Optional[int]
+    id_user = Optional[UserId]
     username = str
     password = str
     gmail = str

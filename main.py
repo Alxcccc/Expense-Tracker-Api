607960d63ff067b6b874d711c6e191d2ed53c0cb
Alxc <alecandres799@gmail.com>
2025-02-17 07:14:53 -0500

    add router users

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from src.app.infrastructure.database.database import DataBase
 from src.app.expenses.infrastructure.routers.expense_router import router as expense_router
+from src.app.users.infrastructure.routers.user_router import router as user_router
 
 app = FastAPI(title=" FastApi - Expense Tracker API")
 
@@ -10,3 +11,4 @@ def startup():
     DataBase.create_engin()
 
 app.include_router(expense_router, prefix="/expense",tags=["Expenses"])
+app.include_router(user_router, prefix="/user",tags=["Users"])

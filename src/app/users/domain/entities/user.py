f3b8f06fc53b7e10f04a2c96969ebe40bd302a1f
Alxc <alecandres799@gmail.com>
2025-02-17 08:04:50 -0500

    add validator gmail user

--- src/app/users/domain/entities/user.py
+++ src/app/users/domain/entities/user.py
@@ -4,15 +4,16 @@ from typing import Optional
 from src.app.users.domain.value_objects.user_id import UserId
 from src.app.users.domain.value_objects.user_username import UserUsername
 from src.app.users.domain.value_objects.user_password import UserPassword
+from src.app.users.domain.value_objects.user_email import UserGmail
 
 class User(BaseModel):
     id_user: Optional[UserId]
     username: UserUsername
     password: UserPassword
-    gmail: EmailStr
+    gmail: UserGmail
     
 class UserCreate(BaseModel):
     username: UserUsername
     password: UserPassword
-    gmail: EmailStr
+    gmail: UserGmail
     
